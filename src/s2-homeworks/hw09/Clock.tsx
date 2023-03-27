9a94ded5b1a1310ac71ffe15609cffa68438dd85
Stanislav Moskalenko <maskal2719@gmail.com>
2023-03-27 13:45:47 +0300

    fix hw9 (getFullYear())

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -33,7 +33,7 @@ function Clock() {
     let month = date.getMonth() < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1
 
     const stringTime = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}` || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = `${day}.${month}.${date.getFullYear()}.` || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringDate = `${day}.${month}.${date.getFullYear()}` || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
     const stringDay = Intl.DateTimeFormat("en-US", {weekday: 'long'}).format(date) || <br/> // пишут студенты
